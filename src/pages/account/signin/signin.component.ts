e3d2b09288e15622035c12a532294aed361e1f42
Shailendra Pal <shailendra.pal@leezair.com>
2017-06-10 19:31:04 +1000

    Complete signin page

--- src/pages/account/signin/signin.component.ts
+++ src/pages/account/signin/signin.component.ts
@@ -2,13 +2,15 @@
 import { Component } from '@angular/core';
 // Ionic
 import { NavController } from 'ionic-angular';
+// App
+import { IUserCredentials } from '../account.model';
 
 @Component({
   selector: 'signin-page-component',
   templateUrl: 'signin.view.html'
 })
 export class SigninPageComponent {
-  credentials: any = {
+  credentials: IUserCredentials = {
     email: '',
     password: ''
   };
@@ -16,6 +18,11 @@ export class SigninPageComponent {
   constructor(
     private _navCtrl: NavController
   ) { }
+
+  // Signin Handler
+  signin(credentials: IUserCredentials) {
+    console.log(credentials);
+  }
 
   // Helper to open a given page
   openPage(page: any) {
