40944193af1cab02c89920cf5ab82a1b2db27821
Shailendra Pal <shailendrapal100@gmail.com>
2017-07-16 17:22:01 +1000

    Close keyboard upon signing up

--- src/pages/account/signup/signup.component.ts
+++ src/pages/account/signup/signup.component.ts
@@ -1,45 +1,46 @@
 // Angular
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 // Ionic
-import { NavController } from 'ionic-angular';
+import { NavController, Keyboard } from "ionic-angular";
 // App
-import { IUserCredentials } from '../account.model';
-import { AccountService } from '../account.service';
-import { ToastService } from '../../core/service';
+import { IUserCredentials } from "../account.model";
+import { AccountService } from "../account.service";
+import { ToastService } from "../../core/service";
 
 export enum DataType {
   PASSWORD,
   STRING,
   BOOLEAN
-};
+}
 @Component({
-  selector: 'signup-page-component',
-  templateUrl: 'signup.view.html'
+  selector: "signup-page-component",
+  templateUrl: "signup.view.html"
 })
 export class SignupPageComponent {
   DataType = DataType;
   passwordDataType: DataType = DataType.PASSWORD;
   credentials: IUserCredentials = {
-    email: '',
+    email: "",
     // password: '',
-    username: ''
+    username: ""
   };
 
   constructor(
     private _accountService: AccountService,
+    private _keyboard: Keyboard,
     private _navCtrl: NavController,
     private _toastService: ToastService
-  ) { }
+  ) {}
 
   // Signup Handler
   signup(credentials: IUserCredentials) {
-    this._accountService.createAccount(credentials)
-      .subscribe(data => {
-        this._toastService.present({
-          message: `Welcome ${credentials.username}!`
-        });
-        this._navCtrl.pop();
+    this._keyboard.close();
+    this._accountService.createAccount(credentials).subscribe(data => {
+      this._toastService.present({
+        message: `Welcome ${credentials.username}!`
       });
+      this._navCtrl.pop();
+    });
   }
 
   // Toggle the visibility of password field
