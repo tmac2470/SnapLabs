c6224963c7e1ccd43545db93ef99b6f6843d58c1
Shailendra Pal <shailendrapal100@gmail.com>
2017-07-16 17:21:39 +1000

    Remove unused variable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,32 @@
 // Angular
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { Http, HttpModule } from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { HttpModule } from "@angular/http";
 // Ionic
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-import { IonicStorageModule } from '@ionic/storage';
-import { BLE } from '@ionic-native/ble'
-import { Keyboard } from '@ionic-native/keyboard';
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
+import { IonicStorageModule } from "@ionic/storage";
+import { BLE } from "@ionic-native/ble";
+import { Keyboard } from "@ionic-native/keyboard";
 // SnapLabApp
-import { SnapLabApp } from './app.component';
-import { AccountModule } from '../pages/account/account.module';
-import { CoreModule } from '../pages/core/core.module';
-import { ConnectModule } from '../pages/connect/connect.module';
-import { HomeModule } from '../pages/home/home.module';
-import { InvestigationsModule } from '../pages/investigations/investigations.module';
-import { InvestigationDetailsModule } from '../pages/investigation-details/investigation-details.module';
-import { AppShellPageComponent } from '../pages/app-shell/app-shell.component';
+import { SnapLabApp } from "./app.component";
+import { AccountModule } from "../pages/account/account.module";
+import { CoreModule } from "../pages/core/core.module";
+import { ConnectModule } from "../pages/connect/connect.module";
+import { HomeModule } from "../pages/home/home.module";
+import { InvestigationsModule } from "../pages/investigations/investigations.module";
+import { InvestigationDetailsModule } from "../pages/investigation-details/investigation-details.module";
+import { AppShellPageComponent } from "../pages/app-shell/app-shell.component";
 
 @NgModule({
-  declarations: [
-    SnapLabApp,
-    AppShellPageComponent
-  ],
+  declarations: [SnapLabApp, AppShellPageComponent],
   imports: [
     BrowserModule,
     HttpModule,
     // Ionic
     IonicModule.forRoot(SnapLabApp, {
-      backButtonText: '',
+      backButtonText: ""
     }),
     IonicStorageModule.forRoot(),
 
@@ -42,10 +39,7 @@ import { AppShellPageComponent } from '../pages/app-shell/app-shell.component';
     InvestigationsModule.forRoot()
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    SnapLabApp,
-    AppShellPageComponent
-  ],
+  entryComponents: [SnapLabApp, AppShellPageComponent],
   providers: [
     BLE,
     Keyboard,
@@ -54,4 +48,4 @@ import { AppShellPageComponent } from '../pages/app-shell/app-shell.component';
     { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule { }
+export class AppModule {}
