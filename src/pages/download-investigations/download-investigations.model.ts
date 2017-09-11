53a2d2e6d076201cfba8bc78c7a626ca085d1cfb
Shailendra Pal <shailendrapal100@gmail.com>
2017-09-11 23:29:54 +1000

    Add more options to search sort

--- src/pages/download-investigations/download-investigations.model.ts
+++ src/pages/download-investigations/download-investigations.model.ts
@@ -2,6 +2,10 @@ export class SearchSortOptions {
   static CREATED = "createdAt";
   static UPDATED = "lastUpdatedAt";
   static CREATOR = "author";
+
+  static CREATEDDESC = "-createdAt";
+  static UPDATEDDESC = "-lastUpdatedAt";
+  static CREATORDESC = "-author";
 }
 
 export interface ISearchParams {
