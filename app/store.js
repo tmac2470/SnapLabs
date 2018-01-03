7de7865ef02840d1e954ae51185420538b8a4b51
Shailendra Pal <shailendra.pal@leezair.com>
2018-01-03 17:11:15 +1100

    Enable devtools

--- app/store.js
+++ app/store.js
@@ -1,9 +1,14 @@
 // Store has been setup
 // Do not modify store unless adding extra middlewares
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import getRootReducer from "./reducers";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default function getStore() {
-  return createStore(getRootReducer());
+  return createStore(
+    getRootReducer(),
+    {},
+    composeEnhancers()
+  );
 }
