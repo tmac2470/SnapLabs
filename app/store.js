8846ea889174fdbefb57436e74b32e6d115ae443
Shailendra Pal <shailendra.pal@leezair.com>
2018-01-05 12:34:28 +1100

    Add redux thunk

--- app/store.js
+++ app/store.js
@@ -1,14 +1,24 @@
 // Store has been setup
 // Do not modify store unless adding extra middlewares
-import { createStore, applyMiddleware, compose } from "redux";
-
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import getRootReducer from "./reducers";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk)
+);
+
 export default function getStore() {
   return createStore(
     getRootReducer(),
     {},
-    composeEnhancers()
+    enhancer
   );
 }
