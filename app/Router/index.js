2ea54f4dbb4e445c3cfcb9ca324824e65d2d866c
Shailendra <shailendrapal100@gmail.com>
2018-01-29 21:30:14 +1100

    Add support for back button in navigation

--- app/Router/index.js
+++ app/Router/index.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import { BackHandler } from "react-native";
 import { connect } from "react-redux";
-import { addNavigationHelpers } from "react-navigation";
+import { addNavigationHelpers, NavigationActions } from "react-navigation";
 
 import AppNavigator from "./routes";
 
 class AppWithNavigation extends Component {
+  componentDidMount() {
+    BackHandler.addEventListener("hardwareBackPress", this.onBackPress);
+  }
+  componentWillUnmount() {
+    BackHandler.removeEventListener("hardwareBackPress", this.onBackPress);
+  }
+  onBackPress = () => {
+    const { dispatch, nav } = this.props;
+    if (nav.index === 0) {
+      return false;
+    }
+    dispatch(NavigationActions.back());
+    return true;
+  };
+
   render() {
     return (
       <AppNavigator
